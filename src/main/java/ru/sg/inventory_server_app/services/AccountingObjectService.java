fcece1e9ff04d370e42c820fa100ed014de346fb
sagil <Ligasy@gmail.com>
2021-06-23 13:20:31 +0500

    Change directory

--- src/main/java/ru/sg/inventory_server_app/services/AccountingObjectService.java
+++ src/main/java/ru/sg/inventory_server_app/services/AccountingObjectService.java
@@ -42,11 +42,11 @@ public class AccountingObjectService {
     @Autowired
     public AccountingObjectService(FileStorageProperties fileStorageProperties) {
         this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath().normalize();
-        try {
-            Files.createDirectories(this.fileStorageLocation);
-        } catch (Exception ex) {
-            throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", ex);
-        }
+//        try {
+//            Files.createDirectories(this.fileStorageLocation);
+//        } catch (Exception ex) {
+//            throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", ex);
+//        }
     }
 
     /*
